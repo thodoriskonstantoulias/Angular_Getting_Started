63dc216f02b1cd3e319877216f8b2a9d8cbbb11a
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-19 20:46:55 +0300

    Created two way data binding, property and event binding

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent{
     pageTitle : string = "Product List";
+    imageWidth : number = 50;
+    imageMargin : number = 2;
+    showImage : boolean = false;
+    listFilter : string = "cart";
     products : any[] = [
         {
             "productId": 2,
@@ -30,4 +34,8 @@ export class ProductListComponent{
             "imageUrl": "assets/images/hammer.png"
         }
     ];
+
+    toggleImage() : void {
+        this.showImage = !this.showImage;
+    }
 }
