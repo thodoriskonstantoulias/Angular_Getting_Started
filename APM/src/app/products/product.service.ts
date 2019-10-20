dac4984fdd068d4f393eb05efeb20936cc38d28f
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-20 21:29:54 +0300

    Created first service

---
+++ APM/src/app/products/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { IProducts } from './product';
+
+@Injectable({
+  providedIn : "root"
+})
+
+export class ProductService {
+
+    getProducts() : IProducts[]{
+        return [
+            {
+                "productId": 2,
+                "productName": "Garden Cart",
+                "productCode": "GDN-0023",
+                "releaseDate": "March 18, 2019",
+                "description": "15 gallon capacity rolling garden cart",
+                "price": 32.99,
+                "starRating": 4.2,
+                "imageUrl": "assets/images/garden_cart.png"
+            },
+            {
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2019",
+                "description": "Curved claw steel hammer",
+                "price": 8.9,
+                "starRating": 4.8,
+                "imageUrl": "assets/images/hammer.png"
+            }
+        ];
+    }
+}
